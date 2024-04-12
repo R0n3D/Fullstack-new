e7c3ffb573a41fcecca63f152950e63ec6ec735e
Ron Fridman <ronfour20@gmail.com>
2024-04-12 14:08:09 +0300

    w

--- jsscript.js
+++ jsscript.js
@@ -1,10 +1,30 @@
-let hello = "hello world";
-console.log(hello);
+// let hello = "hello world";
+// console.log(hello);
 
-let message ="string"
-console.log(message);
+// let message ="string"
+// console.log(message);
 
-let num1 = 1;
-let num2 = 2;
-console.log(num1+num2);
+// let num1 = 1;
+// let num2 = 2;
+// console.log(num1+num2);
 
+// function hello(){
+// console.log("hello");
+// }
+
+// hello();
+// hello();
+// hello();
+
+// function upper(x){
+// console.log(x.toUpperCase());
+// }
+
+// upper("ron");
+
+
+function kafol(){
+    let x =    Math.round(Math.random())
+    console.log(x);
+}
+ kafol();
